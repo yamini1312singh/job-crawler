1c8f7b437da211f5865b32f5a98363dc6640a2f4
yamini1312singh <68637628+yamini1312singh@users.noreply.github.com>
2026-07-14 12:47:32 +0530

    Create job_sources.py

---
+++ scripts/job_sources.py
@@ -0,0 +1,17 @@
+def get_sample_jobs():
+    return [
+        {
+            "company": "Razorpay",
+            "role": "Product Manager",
+            "location": "Bangalore",
+            "link": "https://razorpay.com/careers/",
+            "source": "CAREERS"
+        },
+        {
+            "company": "Postman",
+            "role": "Senior Product Manager",
+            "location": "Remote",
+            "link": "https://www.postman.com/company/careers/",
+            "source": "CAREERS"
+        }
+    ]
