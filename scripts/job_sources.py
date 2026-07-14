7c8c324c9f346f276f2d1675fecf6ee589cb5a05
yamini1312singh <68637628+yamini1312singh@users.noreply.github.com>
2026-07-14 12:58:30 +0530

    Update job_sources.py

--- scripts/job_sources.py
+++ scripts/job_sources.py
@@ -1,17 +1,27 @@
+import requests
+
 def get_sample_jobs():
-    return [
-        {
-            "company": "Razorpay",
-            "role": "Product Manager",
-            "location": "Bangalore",
-            "link": "https://razorpay.com/careers/",
-            "source": "CAREERS"
-        },
-        {
-            "company": "Postman",
-            "role": "Senior Product Manager",
-            "location": "Remote",
-            "link": "https://www.postman.com/company/careers/",
-            "source": "CAREERS"
-        }
-    ]
+
+    jobs = []
+
+    try:
+        url = "https://boards-api.greenhouse.io/v1/boards/postman/jobs"
+
+        response = requests.get(url)
+
+        data = response.json()
+
+        for job in data["jobs"]:
+
+            jobs.append({
+                "company": "Postman",
+                "role": job["title"],
+                "location": job["location"]["name"],
+                "link": job["absolute_url"],
+                "source": "GREENHOUSE"
+            })
+
+    except Exception as e:
+        print(e)
+
+    return jobs
